6893e79eeed7d344015329b7e5071ee568d54c4c
goschtl <ck@novareto.de>
2017-10-27 09:26:43 +0200

    add missing files

---
+++ bgetem/webmag/create.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# # Copyright (c) 2007-2013 NovaReto GmbH
+# # cklinger@novareto.de
+#
+
+from uvc.api import api
+from plone.api import content
+from nva.magazinfolder.interfaces import IMagazinFolder
+from zope.lifecycleevent.interfaces import IObjectCreatedEvent
+
+
+#@api.subscribe(IMagazinFolder, IObjectCreatedEvent) # BBB
+def handle(folder, event):
+    content.create(type="Folder", title="ETEM +", container=folder)
+    content.create(type="Folder", title="TitelStories", container=folder)
+    # --> TitelStories Folder --->
+    #
