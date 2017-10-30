e469d207902ef2fb3106ce4f40b0fa68e555cdd8
goschtl <ck@novareto.de>
2017-10-30 07:08:48 +0100

    checkpoint

--- bgetem/webmag/views.py
+++ bgetem/webmag/views.py
@@ -1,27 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from Products.CMFCore.utils import getToolByName
-from five import grok
-from plone import api as ploneapi
-from plone.app.contenttypes.interfaces import IDocument
-from plone.app.layout.globals.interfaces import IViewView
-from plone.memoize import ram
-from time import time
-from uvc.api import api
-from uvc.shards.components import ShardsAsViews
-from uvc.shards.interface import IShardedView
-from zope.interface import Interface
+import urllib
 
+
+from five import grok
+from uvc.api import api
+from plone import api as ploneapi
 from .layout import BSPage as Page
+from zope.interface import Interface
+from Products.CMFCore.utils import getToolByName
+from plone.app.contenttypes.interfaces import IDocument
 from nva.magazinfolder.interfaces import IAnonymousLayer
+from plone.app.layout.globals.interfaces import IViewView
 
 
 api.templatedir('templates')
-
-
-class SimpleNewspaperView(api.View):
-    api.name('newspaperview')
-    api.context(Interface)
 
 
 class NewspaperView(Page):
@@ -69,8 +62,8 @@ class Bildrechte(Page):
 
     def getImageBrains(self):
         folderid = self.context.aq_parent.id
-        path = '/magazin/%s/medien-dieser-ausgabe' %folderid
-        return self.portal_catalog(path=path, portal_type = 'Image')
+        path = '/magazin/%s/medien-dieser-ausgabe' % folderid
+        return self.portal_catalog(path=path, portal_type='Image')
 
     def update(self):
         brains = self.getImageBrains()
@@ -101,23 +94,14 @@ class Kompaktarchiv(Page):
                    ]
         archiv = []
         for i in folders:
-           portal = ploneapi.portal.get()
-           obj = getattr(portal, i)
-           magazin = {}
-           if obj:
-               magazin['title'] = obj.description
-               magazin['url'] = obj.absolute_url()+'/index.html/kompakttitelview'
-               magazin['imgurl'] = obj.absolute_url() + '/medien-dieser-ausgabe/titelbild/@@images/image/thumb'
-               print magazin['imgurl']
-               titelobj = getattr(obj, u'titelstory')
-               magazin['storytitle'] = titelobj.title
-               archiv.append(magazin)
+            portal = ploneapi.portal.get()
+            obj = getattr(portal, i)
+            magazin = {}
+            if obj:
+                magazin['title'] = obj.description
+                magazin['url'] = obj.absolute_url()+'/index.html/kompakttitelview'
+                magazin['imgurl'] = obj.absolute_url() + '/medien-dieser-ausgabe/titelbild/@@images/image/thumb'
+                titelobj = getattr(obj, u'titelstory')
+                magazin['storytitle'] = titelobj.title
+                archiv.append(magazin)
         self.archiv = archiv
-
-#class DocumentView(api.Page):
-#    api.implements(IShardedView)
-#    api.context(Interface)
-#    shards = ShardsAsViews()
-#
-#    def update(self):
-#        self.mydoc = self.request.get('mydoc')
